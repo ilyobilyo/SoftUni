3ab2ead5df052144c6ec0c81c4078510615a1e98
ilyobilyo <ilito20000@gmail.com>
2022-06-16 17:13:38 +0300

    C# OOP

---
+++ C# OOP/C# OOP Exam 2/NavalVessels/NavalVessels/Models/Submarine.cs
@@ -0,0 +1,67 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double InitialArmorThickness = 200;
+
+        private bool submarineMode;
+
+        public Submarine(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, InitialArmorThickness)
+        {
+        }
+
+        public bool SubmergeMode 
+        {
+            get { return submarineMode; }
+            private set { submarineMode = false; }
+        }
+
+
+        public override void RepairVessel()
+        {
+            if (ArmorThickness < InitialArmorThickness)
+            {
+                ArmorThickness = InitialArmorThickness;
+            }
+        }
+
+        public void ToggleSubmergeMode()
+        {
+            if (SubmergeMode)
+            {
+                SubmergeMode = false;
+                MainWeaponCaliber -= 40;
+                Speed += 4;
+            }
+            else
+            {
+                SubmergeMode = true;
+                MainWeaponCaliber += 40;
+                Speed -= 4;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            if (SubmergeMode)
+            {
+                sb.AppendLine($" *Submerge mode: ON");
+
+            }
+            else
+            {
+                sb.AppendLine($" *Submerge mode: OFF");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
