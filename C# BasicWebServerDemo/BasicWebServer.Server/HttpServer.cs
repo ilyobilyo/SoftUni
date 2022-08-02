dd3564f55d85fb36f4e702472157307a31c85659
ilyobilyo <ilito20000@gmail.com>
2022-08-02 17:32:29 +0300

    BasicWebServerDemo

---
+++ C# BasicWebServerDemo/BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,122 @@
+﻿using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+using BasicWebServer.Server.Routing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+        private readonly RoutingTable routingTable;
+
+        public HttpServer(string ipAddress, int port,
+            Action<IRoutingTable> routingTableConfiguration)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress, port);
+
+            routingTableConfiguration(this.routingTable = new RoutingTable());
+        }
+
+        public HttpServer(int port, Action<IRoutingTable> routingTable)
+            : this("127.0.0.1", port, routingTable)
+        {
+
+        }
+
+        public HttpServer(Action<IRoutingTable> routingTable)
+            : this(8080, routingTable)
+        {
+
+        }
+
+        public async Task Start()
+        {
+            this.serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests ...");
+
+            while (true)
+            {
+                var connection = await serverListener.AcceptTcpClientAsync();
+
+                _ = Task.Run(async () =>
+                {
+                    var networkStream = connection.GetStream();
+
+                    var content = "Hello from the server!";
+
+                    var requestText = await this.ReadRequest(networkStream);
+
+                    Console.WriteLine(requestText);
+
+                    var request = Request.Parse(requestText);
+
+                    var response = this.routingTable.MatchRequest(request);
+
+                    AddSession(request, response);
+
+                    await WriteResponse(networkStream, response);
+
+                    connection.Close();
+
+                });
+            }
+        }
+
+        private static void AddSession(Request request, Response response)
+        {
+            var sessionExists = request.Session.ContainsKey(Session.SessionCurrentDateKey);
+
+            if (!sessionExists)
+            {
+                request.Session[Session.SessionCurrentDateKey] = DateTime.Now.ToString();
+
+                response.Cookies.Add(Session.SessionCookieName, request.Session.Id);
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+
+            var buffer = new byte[bufferLength];
+
+            var totalBytes = 0;
+
+            var sb = new StringBuilder();
+
+            do
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                sb.AppendLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+            } while (networkStream.DataAvailable);
+
+            return sb.ToString();
+        }
+
+        private async Task WriteResponse(NetworkStream networkStream, Response response)
+        {
+            var responseBytes = Encoding.UTF8.GetBytes(response.ToString());
+
+            await networkStream.WriteAsync(responseBytes);
+        }
+    }
+}
---
+++ C# BasicWebServerDemo/BasicWebServer.Server/Routing/RoutingTable.cs
@@ -0,0 +1,63 @@
+﻿using BasicWebServer.Server.Common;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, Dictionary<string, Func<Request, Response>>> routes;
+
+        public RoutingTable()
+        {
+            this.routes = new()
+            {
+                [Method.GET] = new(),
+                [Method.POST] = new(),
+                [Method.PUT] = new(),
+                [Method.DELETE] = new(),
+            };
+
+
+        }
+
+        public IRoutingTable Map(Method method, string path, Func<Request, Response> responseFuntion)
+        {
+            Guard.AgainstNull(path, nameof(path));
+            Guard.AgainstNull(responseFuntion, nameof(responseFuntion));
+
+            this.routes[method][path] = responseFuntion;
+
+            return this;
+        }
+
+
+        public IRoutingTable MapGet(string path, Func<Request, Response> responseFuntion)
+            => Map(Method.GET, path, responseFuntion);
+
+        public IRoutingTable MapPost(string path, Func<Request, Response> responseFuntion)
+            => Map(Method.POST, path, responseFuntion);
+
+        public Response MatchRequest(Request request)
+        {
+            var requestMethod = request.Method;
+
+            var requestUrl = request.Url;
+
+            if (!this.routes.ContainsKey(requestMethod)
+                || !this.routes[requestMethod].ContainsKey(requestUrl))
+            {
+                return new NotFoundResponse();
+            }
+
+            var responseFunction = this.routes[requestMethod][requestUrl];
+
+            return responseFunction(request);
+        }
+    }
+}
