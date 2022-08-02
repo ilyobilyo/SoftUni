9b7b575efef9e35b57450e1f5a9b416f08215552
ilyobilyo <ilito20000@gmail.com>
2022-08-02 15:18:10 +0300

    C# Web Basic Chronometer

---
+++ C# WebBasics/Chronometer/Chronometer/Program.cs
@@ -0,0 +1,54 @@
+﻿using ChronometerTest;
+
+var chronometer = new Chronometerr();
+
+while (true)
+{
+    string line = Console.ReadLine();
+
+    if (line == "exit")
+    {
+        break;
+    }
+
+    if (line == "start")
+    {
+        Task.Run(() =>
+        {
+            chronometer.Start();
+        });
+    }
+    else if (line == "stop")
+    {
+        chronometer.Stop();
+    }
+    else if (line == "lap")
+    {
+        Console.WriteLine(chronometer.Lap());
+    }
+    else if (line == "laps")
+    {
+        if (chronometer.Laps.Count == 0)
+        {
+            Console.WriteLine("Laps: no laps");
+            continue;
+        }
+
+        Console.WriteLine("Laps: ");
+
+        for (int i = 0; i < chronometer.Laps.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {chronometer.Laps[i]}");
+        }
+    }
+    else if (line == "reset")
+    {
+        chronometer.Reset();
+    }
+    else if (line == "time")
+    {
+        Console.WriteLine(chronometer.GetTime);
+    }
+}
+
+chronometer.Stop();
---
+++ C# WebBasics/Chronometer/TestAsync/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<string> list = new List<string>();
+
+            var task1 = Task.Run(() =>
+            {
+                for (int i = 1; i <= 15000000; i++)
+                {
+                    list.Add($"{i}");
+                }
+
+            });
+
+            task1.Wait();
+
+            var task2 = Task.Run(() =>
+            {
+                for (int i = 200; i < 40000000; i += 2)
+                {
+                    list.Add($"Async => {i}");
+                }
+
+                //Console.WriteLine(String.Join(": ", list));
+            });
+
+            string v = Console.ReadLine();
+
+            Console.WriteLine(list.Count);
+        }
+    }
+}
