90946fb74491948f1d91faa72d49decbcd205f74
ilyobilyo <ilito20000@gmail.com>
2022-08-02 15:19:38 +0300

    BasicWebServer

---
+++ C# WebBasics/HttpServer/BasicWebServer.Server/Responses/Response.cs
@@ -0,0 +1,54 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class Response
+    {
+        public Response(StatusCode statusCode)
+        {
+            StatusCode = statusCode;
+            Headers.Add(Header.Server, "My Web Server");
+            Headers.Add(Header.Date, $"{DateTime.UtcNow:r}");
+        }
+
+        public StatusCode StatusCode { get; init; }
+
+        public HeaderCollection Headers { get; } = new HeaderCollection();
+
+        public CookieCollection Cookies { get; } = new CookieCollection();
+
+        public string Body { get; set; }
+
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}");
+
+            foreach (var header in Headers)
+            {
+                result.AppendLine(header.ToString());
+            }
+
+            foreach (var cookie in Cookies)
+            {
+                result.AppendLine($"{Header.SetCookie}: {cookie}");
+            }
+
+            result.AppendLine();
+
+            if (!string.IsNullOrEmpty(this.Body))
+            {
+                result.Append(this.Body);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ C# WebBasics/HttpServer/BasicWebServer.Server/Routing/RoutingTableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Routing
+{
+    public static class RoutingTableExtensions
+    {
+        public static IRoutingTable MapGet<TController>(
+            this IRoutingTable rountingTable,
+            string path,
+            Func<TController, Response> controllerFunction) where TController : Controller
+            => rountingTable.MapGet(path, request => controllerFunction(CreateController<TController>(request)));
+
+        public static IRoutingTable MapPost<TController>(
+            this IRoutingTable rountingTable,
+            string path,
+            Func<TController, Response> controllerFunction) where TController : Controller
+            => rountingTable.MapPost(path, request => controllerFunction(CreateController<TController>(request)));
+
+        private static TController CreateController<TController>(Request request)
+            => (TController)Activator.CreateInstance(typeof(TController), new[] {request});
+    }
+}
